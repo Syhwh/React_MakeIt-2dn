8a760fbb4cbd122c4a03c417e15f8056e1c0114a
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2019-10-22 13:49:44 -0500

    Add incremment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+   
+  }
+  handleClick =()=> {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value" >{this.state.count}</span>
+        <button id="inc" onClick={this.handleClick}  >Incrementa</button>
       </div>
     );
   }
